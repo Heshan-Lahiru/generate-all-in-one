3e98c97413eaab6f2a89429dc2949f88fa0a6551
Lahiru heshan <lahiruheshan454@gmail.com>
2024-09-12 22:11:10 +0530

    a

--- generate-all/src/App.js
+++ generate-all/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Pdf from './components/pdf/pdf';
 
-
-
 function App() {
   return (
     <Router>
